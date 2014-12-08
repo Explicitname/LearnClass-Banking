abaf79f17778957fed3a03226df681e9a04979d4
explicitname <explicit6911@gmail.com>
2014-12-08 14:55:33 -0500

    Progressing through the videos on Pluralsight

--- Transaction.h
+++ Transaction.h
@@ -8,7 +8,18 @@
 #ifndef TRANSACTION_H_
 #define TRANSACTION_H_
 
+#include <string>
 
+class Transaction
+{
+private:
+	int amount;
+	std::string type;
+
+public:
+	Transaction(int amt, std::string kind);
+	std::string Report();
+};
 
 
 
